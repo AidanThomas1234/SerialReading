297d952f62f5dd5cb947074d6ccd5195fd542453
Aidan Thomas <78534308+AidanThomas1234@users.noreply.github.com>
2024-05-22 11:35:32 +0100

    Now working

--- SerialReading.py
+++ SerialReading.py
@@ -11,9 +11,7 @@ import csv
 from docx import Document
 from docx.shared import Pt
 from docx.enum.table import WD_TABLE_ALIGNMENT
-
-
-
+import gc
 
 # Extracts the number from a string
 def extract_number(input_string):
@@ -69,13 +67,11 @@ def print_file_to_word_doc(file_path, report_date):
             cell.paragraphs[0].alignment = WD_TABLE_ALIGNMENT.CENTER
 
     doc.save(f'report_{report_date.strftime("%Y-%m-%d_%H-%M-%S")}.docx') # Save Word document with date and time
-    pathend=(f'report_{report_date.strftime("%Y-%m-%d_%H-%M-%S")}.docx')
-    wrd_file_path = "C:/Users/Projects.PURNHOUSEFARM/"+ pathend
+    pathend = (f'report_{report_date.strftime("%Y-%m-%d_%H-%M-%S")}.docx')
+    wrd_file_path = "C:/Users/Projects.PURNHOUSEFARM/" + pathend
     print(wrd_file_path)
 
-
-
-#Used to prints files 
+# Used to print files 
 def print_file_to_printer(file_path):
     printer_name = "Alistairsprinter"  # Adjust to your printer
     hdc = win32ui.CreateDC()
@@ -117,106 +113,43 @@ mycursor = mydb.cursor()
 
 exit_flag = False
 
-
-def read_serial_data(port, baud_rate,ser):
-    try:
-        global exit_flag
-        ser = serial.Serial(port, baud_rate)  # Open the serial port
-        read_count = 0  # Counter to track the number of readings
-        
-        # Rest of your function code...
-
-    except Exception as e:
-        handle_error=(f"An error occurred while reading serial data: {e}")
-    finally:
-        if ser.is_open:
-            ser.close()  # Close the serial port in the finally block
+def read_serial_data(port, baud_rate, ser=None):
     global exit_flag
-    ser = serial.Serial(port, baud_rate)
-
-    read_count = 0  # Counter to track the number of readings
-    
-    # Prompt for user input at the start
-    while True:
-        user_input = input("Please enter the batch number to begin : ")
-        if user_input == '0':
-            menu(port, baud_rate,ser)
-            return  
-        1
-        try:
-            user_input = int(user_input)  # Try to convert the input to an integer
-            break  # Break out of the2 loop if conversion is successful
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-  
-  
-    product = None  # Initialize product to None
-
-    product_input = int(input("""Enter the Product Type:\n
-    1) Skins\n
-    2) Sticks\n"""))
-
-    if product_input == 1:
-        product = "Skins"  # Assign "Skins" to product
-    elif product_input == 2:
-        product = "Sticks"  # Assign "Sticks" to product
-    else:
-        print("Invalid Input. Restarting...")
-        read_serial_data(port, baud_rate,ser)
-
+    if ser is None:
+        ser = serial.Serial(port, baud_rate)  # Open the serial port
+        print(f"Opened serial port {port}")
 
     try:
+        read_count = 0  
+
+        batch_number = get_batch_number()
+        if batch_number is None:  
+            return
+
+        product = get_product_type()
+        if product is None: 
+            return
+
         while True:
             if exit_flag:
                 print("Exiting to menu...")
-                menu(port, baud_rate,ser)
-                return  # Exit to menu
+                break  # Exit loop
 
             if ser.in_waiting > 0:
                 data = ser.readline().strip()
                 decoded_data = data.decode("utf-8")
 
                 if decoded_data.startswith("Gross"):
-                    # print_to_printer(decoded_data)
                     number = extract_number(decoded_data)
-
-                    # Increment the read count
                     read_count += 1
-                    
-                    # Prompt for user input every 22 readings
+
                     if read_count % 22 == 0:
-                        while True:
-                            user_input = input("Please enter the batch number to begin (or enter 0 to exit): ")
-                            if user_input == '0':
-                                menu(port, baud_rate,ser)
-                                return  # Exit to menu
-                            
+                        batch_number = get_batch_number()
+                        product = get_product_type()
 
-                            product = None  # Initialize product to None
-
-                            product_input = int(input("""Enter the Product Type:\n
-                            1) Skins\n
-                            2) Sticks\n"""))
-
-                            if product_input == 1:
-                                product = "Skins"  # Assign "Skins" to product
-                            elif product_input == 2:
-                                product = "Sticks"  # Assign "Sticks" to product
-                            else:
-                                print("Invalid Input. Restarting...")
-                                read_serial_data(port, baud_rate,ser)
-
-                
-                            try:
-                                user_input = int(user_input)  # Try to convert the input to an integer
-                                break  # Break out of the loop if conversion is successful
-                            except ValueError:
-                                print("Invalid input. Please enter an integer.")
-                    
-                    # Insert into the database
                     current_time = datetime.now()
                     sql = "INSERT INTO `LakesWeighHead` (`BagID`, `GrossWeight`, `DateandTime`, `BatchNumb`, `ProductType`) VALUES ('', %s, %s, %s, %s)"
-                    val = (number, current_time, user_input, product)
+                    val = (number, current_time, batch_number, product)
                     mycursor.execute(sql, val)
                     mydb.commit()
 
@@ -224,34 +157,67 @@ def read_serial_data(port, baud_rate,ser):
                     mycursor.execute(idsql)
                     CurrentID = mycursor.fetchone()
                     most_recent_id = CurrentID[0]
-                   
-                    print(f"\n\nBatch:{user_input}   Weight:{number}    Product:{product}    BagID:{most_recent_id}       Date and time:{current_time}\n")
 
-                    # This line should be commented during testing to prevent having 5000 pages 
-                    #print_to_printer(f"\n\nBatch:{user_input}   Weight:{number}    Product:{product}    BagID:{most_recent_id}       Date and time:{current_time}\n")
-            
+                    print(f"\n\nBatch: {batch_number}   Weight: {number}    Product: {product}    BagID: {most_recent_id}       Date and time: {current_time}\n")
+
+                    # Uncomment to print to the printer
+                    # print_file_to_printer(f"\n\nBatch: {batch_number}   Weight: {number}    Product: {product}    BagID: {most_recent_id}       Date and time: {current_time}\n")
+        
+
             time.sleep(0.1)  # Small delay to prevent CPU overload
+
+    except serial.SerialException as e:
+        print(f"Serial exception: {e}")
 
     except KeyboardInterrupt:
         print("Interrupted by user. Exiting to menu.")
-        menu(port, baud_rate)
-        return  # Exit to menu
 
     finally:
-        ser.close()  # Close the serial connection
-def update(port, baud_rate):
-    print("Update is used if a bag is broken or incorrectly weighed\n")
-    print("Please Enter the Bag ID for the bag that needs reweighing\n")
+        if ser and ser.is_open:
+            ser.flush()  # Flush the serial port
+            ser.close()  # Close the serial connection
+            del ser
+            gc.collect()
+            time.sleep(2)  # Wait for 2 seconds before re-opening
+            print(f"Closed serial port {port}")
+
+            # Reopen menu
+            menu(port, baud_rate)
+
+
+def get_batch_number():
     while True:
-        bag_id = input("--> ")
+        user_input = input("Please enter the batch number to begin (or enter 0 to exit): ")
+        if user_input == '0':
+            return None
         try:
-            bag_id = int(bag_id)  # Ensure the input is an integer
-            break  # Break out of the loop if conversion is successful
+            return int(user_input)
         except ValueError:
             print("Invalid input. Please enter an integer.")
+
+def get_product_type():
+    while True:
+        try:
+            product_input = int(input("""Enter the Product Type:\n
+            1) Skins\n
+            2) Sticks\n"""))
+            if product_input == 1:
+                return "Skins"
+            elif product_input == 2:
+                return "Sticks"
+            else:
+                print("Invalid input. Please enter 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+def update(port, baud_rate, ser):
+    print("Update is used if a bag is broken or incorrectly weighed\n")
+    print("Please Enter the Bag ID for the bag that needs reweighing\n")
     
-    ser = serial.Serial(port, baud_rate)
-    print("Please place the bag on the scale to get the new weight...")
+    while True:
+        bag_id = input("-->")
+        if bag_id:
+            break
 
     try:
         while True:
@@ -261,28 +227,41 @@ def update(port, baud_rate):
 
                 if decoded_data.startswith("Gross"):
                     new_weight = extract_number(decoded_data)
+
                     if new_weight is not None:
-                        # Update the weight in the database
                         sql = "UPDATE `LakesWeighHead` SET `GrossWeight` = %s WHERE `BagID` = %s"
                         val = (new_weight, bag_id)
                         mycursor.execute(sql, val)
                         mydb.commit()
-                        print(f"Bag ID {bag_id} updated with new weight {new_weight}.")
-                        break  # Break the loop after updating the weight
 
-            time.sleep(0.1)  # Small delay to prevent CPU overload
+                        sql = "SELECT `BatchNumb`, `ProductType` FROM `LakesWeighHead` WHERE `BagID` = %s"
+                        val = (bag_id,)
+                        mycursor.execute(sql, val)
+                        result = mycursor.fetchone()
 
-    except KeyboardInterrupt:
-        print("Interrupted by user. Exiting to menu.")
-    
+                        if result:
+                            batch_number, product = result
+                            current_time = datetime.now()
+                            print(f"Updated Bag ID: {bag_id}, New Weight: {new_weight}, Batch Number: {batch_number}, Product: {product}, Date and Time: {current_time}")
+                            break  # Exit after a successful update
+                    else:
+                        print("Failed to extract a valid number from the scale data. Please try again.")
+
+            time.sleep(0.1)
+
+    except serial.SerialException as e:
+        print(f"Serial exception: {e}")
+
     finally:
-        ser.close()  # Close the serial connection
-        menu(port, baud_rate,ser)  # Return to menu
+        menu(port, baud_rate, ser)  # Return to the main menu
 
 
-def previous_day_reports(port, baud_rate):
+
+
+
+def previous_day_reports(port, baud_rate,ser):
     today = datetime.now().date()
-    days = [(today - timedelta(days=i)) for i in range(1, 21)]  # Last 20 days
+    days = [today - timedelta(days=i) for i in range(1, 21)]  # Last 20 days
 
     print("Select a day to view the report:")
     for i, day in enumerate(days, 1):
@@ -319,51 +298,65 @@ def previous_day_reports(port, baud_rate):
             for row in results:
                 csv_writer.writerow(row)  
         print(f"Previous day report saved to {csv_filename}.")
-        csv_file_path = "C:/Users/Projects.PURNHOUSEFARM/"+ csv_filename
+        csv_file_path = "C:/Users/Projects.PURNHOUSEFARM/" + csv_filename
         
-       
-        print_file_to_word_doc(csv_file_path,selected_date)
-        print("File Saved to:",csv_file_path)
+        print_file_to_word_doc(csv_file_path, selected_date)
+        print("File Saved to:", csv_file_path)
         
     else:
         print(f"No entries found for {selected_date}.")
 
-    menu(port, baud_rate,ser) 
-def exit_listener():
+    menu(port, baud_rate, ser)
+
+def exit_listener(port, baud_rate):
     global exit_flag
-    print("Press '0' to exit.")
+    print("Press '0' to exit to the menu.")
     while True:
         if msvcrt.kbhit():  
             input_char = msvcrt.getch().decode('utf-8')
             if input_char == '0':
                 exit_flag = True
-                break
-def menu(port, baud_rate,ser):
-    print("")
-    print("""\n
-             Weigh Head System\n
-            1) Start Printing\n
-            2) Update a Field\n
-            3) Reports\n
-            """)
-    
+                return
+
+
+
+
+
+
+
+
+def menu(port, baud_rate, ser=None):
+    print("\nWeigh Head System\n")
+    print("1) Start Printing\n")
+    print("2) Update a Field\n")
+    print("3) Reports\n")
+
+    if ser is None:
+        ser = serial.Serial(port, baud_rate)  # Open the serial port
+        print(f"Opened serial port {port}")
+
     MenuOption = input("-->")
     if MenuOption == "1":
-        exit_thread = threading.Thread(target=exit_listener)
+        global exit_flag
+        exit_flag = False
+        exit_thread = threading.Thread(target=exit_listener, args=(port, baud_rate))
         exit_thread.daemon = True
         exit_thread.start()
-        read_serial_data(port, baud_rate,ser)
+        read_serial_data(port, baud_rate, ser)
     elif MenuOption == "2":
-        update(port, baud_rate)
+        update(port, baud_rate,ser)
     elif MenuOption == "3":
-        previous_day_reports(port, baud_rate)
+        previous_day_reports(port, baud_rate,ser)
     else:
         print("Invalid Input")
-        menu(port, baud_rate,ser)  # Return to menu for invalid input
+        menu(port, baud_rate, ser)  # Return to menu for invalid input
 
 if __name__ == "__main__":
-    ser=""
     port = 'COM3'  # Set your COM port
     baud_rate = 9600  # Set the baud rate
 
-    menu(port, baud_rate,ser)
+    menu(port, baud_rate)
+
+    # Add this line to wait for user input before closing the window
+    input("Press Enter to exit...")
+
